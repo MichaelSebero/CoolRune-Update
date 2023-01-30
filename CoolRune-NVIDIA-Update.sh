ca1065ee5306442c8546e72d33b3c2c930f3bfb5
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-01-30 15:45:26 -0500

    Add files via upload

---
+++ CoolRune-NVIDIA-Update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# License: GNU GPLv3
+
+read -rep $'Make sure to backup your passwords and bookmarks before updating CoolRune, press [ENTER] to continue. '
+
+su -c ' pacman -Syyu --noconfirm --needed && update-grub &&  mkdir /home/CoolRune-Files && cd /home/CoolRune-Files && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Files.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-NVIDIA-Patch.7z && curl -RO https://raw.githubusercontent.com/MichaelSebero/CoolRune-Files/master/CoolRune-Dotfiles.7z && chattr -i /etc/hosts && chattr -i /etc/resolv.conf && 7z x CoolRune-Files.7z -o/ -y && 7z x CoolRune.7z -o/ -y && 7z x CoolRune-NVIDIA-Patch.7z -o/ -y && 7z x CoolRune-Dotfiles.7z -o/home/$USER -y && chattr +i /etc/hosts && chattr +i /etc/resolv.conf && rm -rf /home/CoolRune-Files && chmod 777 /home/$USER/.librewolf -R && reboot '
